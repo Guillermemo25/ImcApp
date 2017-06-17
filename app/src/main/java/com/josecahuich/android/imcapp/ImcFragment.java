c60052389658b422562f814df0820537d8029ead
Guillermemo25 <memocc716@gmail.com>
2017-06-16 23:16:20 -0500

    Proyecto ImApp, que calcula el Índice de Masa Corporal (IMC)

--- app/src/main/java/com/josecahuich/android/imcapp/ImcFragment.java
+++ app/src/main/java/com/josecahuich/android/imcapp/ImcFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Guillermo on 16/06/2017.
@@ -13,15 +16,53 @@ import android.view.ViewGroup;
 
 public class ImcFragment extends android.support.v4.app.Fragment {
 
+
+    EditText mCampoPeso;
+    EditText mCampoEstatura;
+    Button mBotonCalcular;
+    Button mBotonLimpiar;
+    TextView mEtiquetaImc;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragmento_imc, container, false);
+
+        mCampoPeso = (EditText) v.findViewById(R.id.campo_peso);
+        mCampoEstatura = (EditText) v.findViewById(R.id.campo_estatura);
+        mBotonCalcular = (Button) v.findViewById(R.id.boton_calcular);
+        mBotonLimpiar = (Button) v.findViewById(R.id.boton_limpiar);
+        mEtiquetaImc = (TextView) v.findViewById(R.id.etiqueta_imc);
+
+        mBotonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = mCampoPeso.getText().toString();
+                double peso = Double.parseDouble(s);
+                s = mCampoEstatura.getText().toString();
+                double estatura = Double.parseDouble(s);
+                double imc = peso / (estatura * estatura);
+                s = String.format("%2.2f", imc);
+                mEtiquetaImc.setText(s);
+            }
+        });
+
+        mBotonLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCampoPeso.setText("");
+                mCampoEstatura.setText("");
+                mEtiquetaImc.setText("0.0");
+            }
+        });
+
         return v;
+
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+
+    };
 }
